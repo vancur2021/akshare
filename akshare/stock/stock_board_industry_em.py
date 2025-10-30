dcf060d8a4bc9b8c56dbb300bcd5d95ac78a95e0
kiang <vancur2021@gmail.com>
2025-10-30 18:09:18 +0800

    修改 __stock_board_industry_name_em

--- akshare/stock/stock_board_industry_em.py
+++ akshare/stock/stock_board_industry_em.py
@@ -23,20 +23,22 @@ def __stock_board_industry_name_em() -> pd.DataFrame:
     :return: 行业板块-名称
     :rtype: pandas.DataFrame
     """
-    url = "https://17.push2.eastmoney.com/api/qt/clist/get"
+    url = "https://push2.eastmoney.com/api/qt/clist/get"
     params = {
         "pn": "1",
         "pz": "100",
         "po": "1",
         "np": "1",
-        "ut": "bd1d9ddb04089700cf9c27f6f7426281",
-        "fltt": "2",
+        "ut": "fa5fd1943c7b386f172d6893dbfba10b",
+        "fltt": "1",
         "invt": "2",
         "fid": "f3",
         "fs": "m:90 t:2 f:!50",
         "fields": "f1,f2,f3,f4,f5,f6,f7,f8,f9,f10,f12,f13,f14,f15,f16,f17,f18,f20,f21,"
         "f23,f24,f25,f26,f22,f33,f11,f62,f128,f136,f115,f152,f124,f107,f104,f105,"
         "f140,f141,f207,f208,f209,f222",
+        "dect": "1",
+        "wbp2u":"|0|0|0|web"
     }
     temp_df = fetch_paginated_data(url, params)
     temp_df.columns = [
